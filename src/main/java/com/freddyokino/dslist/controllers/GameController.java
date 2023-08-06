9050c66609bdc3aff3af47c6d375a73846c9e39d
DevFreddy <devfreddyokino@hotmail.com>
2023-08-06 11:02:39 -0400

    get game by id

--- src/main/java/com/freddyokino/dslist/controllers/GameController.java
+++ src/main/java/com/freddyokino/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.freddyokino.dslist.dto.GameDTO;
 import com.freddyokino.dslist.dto.GameMinDTO;
 import com.freddyokino.dslist.services.GameService;
 
@@ -21,6 +23,14 @@ public class GameController {
 	public List<GameMinDTO> findAll(){
 		
 		List<GameMinDTO> result=  gameService.findAll();
+		return result;
+	}
+	
+
+	@GetMapping(value="/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		
+		GameDTO result=  gameService.findById(id);
 		return result;
 	}
 }
